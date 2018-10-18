0037af0f2ae4572bf2a46368d7d7fc804a16925f
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-10-18 21:01:02 +0800

    Ready to test on cloud.

--- funcPackage/json_reader.py
+++ funcPackage/json_reader.py
@@ -5,11 +5,12 @@ import logging
 logging.basicConfig(filename='../logs/MQreceiver.log',format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]', filemode='w',level = logging.ERROR,datefmt='%Y-%m-%d %I:%M:%S %p')
 
 
-# 读取默认位置的配置文件
+# 读取默认位置的配置文件 特别注意测试和运行时刻的工作路径!
 def get_conf(file_path="../MQOperator_conf.json"):
     try:
         return get_json(file_path)
     except:
+        logging.error("Configration file can not be loaded. (file_path = %s)" % file_path)
         raise IOError
 
 # 读取json格式的文件，返回一个dict类型
@@ -20,7 +21,7 @@ def get_json(file_path):
         f.close()
         return temp
     except:
-        logging.ERROR("Configration file can not be loaded.")
+        logging.error("json format in this file is not correct. (file_path = %s) " % file_path)
         raise IOError
 
 
